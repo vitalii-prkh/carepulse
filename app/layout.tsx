707e3ba73c8382efe0bf6a74bdf03321ffeb17b9
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-09-17 12:32:59 +0300

    Add UI components and theme provider integration
    - Introduced `@radix-ui/react-slot`, `class-variance-authority`, `lucide-react`, and `next-themes` as new dependencies for enhanced UI and theming support.
    - Added `components.json` for UI component and configuration management.
    - Created reusable `ThemeProvider` and `Button` components.
    - Updated `app/layout.tsx` and `app/page.tsx` to integrate `ThemeProvider` and the `Button` component.
    - Updated `package.json` and `package-lock.json` with the new dependencies.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from "next";
 import {Plus_Jakarta_Sans} from "next/font/google";
 import {cn} from "@/lib/utils";
+import {ThemeProvider} from "@/components/theme-provider";
 import "./globals.css";
 
 const fontSans = Plus_Jakarta_Sans({
@@ -30,7 +31,12 @@ export default function RootLayout({
           fontSans.variable,
         )}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import {Button} from "@/components/ui/button";
+
+function Home() {
   return (
     <div>
-        <h1 className="text-3xl underline">Home</h1>
+      <h1 className="text-3xl text-white underline">Home</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
+
+export default Home;
