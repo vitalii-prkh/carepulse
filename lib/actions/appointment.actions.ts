99ff5571e85bce1ebf53fd7803267df2c103e688
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-09-19 16:53:22 +0300

    Add `getAppointment` action and success page layout enhancements
    - Implemented `getAppointment` action in `appointment.actions.ts` for fetching appointment details.
    - Enhanced `PageAppointmentSuccess` component with UI improvements and detailed appointment information display.

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -1,11 +1,11 @@
 "use server";
 
+import {ID, Query} from "node-appwrite";
 import {
   APPOINTMENT_COLLECTION_ID,
   DATABASE_ID,
   databases,
 } from "@/lib/appwrite.config";
-import {ID} from "node-appwrite";
 import {parseStringify} from "@/lib/utils";
 
 export async function createAppointment(data: CreateAppointmentParams) {
@@ -18,6 +18,20 @@ export async function createAppointment(data: CreateAppointmentParams) {
     });
 
     return parseStringify(appointment);
+  } catch (error: unknown) {
+    console.log(error);
+  }
+}
+
+export async function getAppointment(appointmentId: string) {
+  try {
+    const patients = await databases.listDocuments({
+      databaseId: DATABASE_ID,
+      collectionId: APPOINTMENT_COLLECTION_ID,
+      queries: [Query.equal("$id", appointmentId)],
+    });
+
+    return parseStringify(patients.documents[0]);
   } catch (error: unknown) {
     console.log(error);
   }
