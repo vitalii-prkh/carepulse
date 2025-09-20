6c6077d765ca143bd1d493c0553ef8a477ba5c71
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-09-20 18:58:55 +0300

    Add `ModalAppointment` component for scheduling and cancelling appointments
    - Introduced reusable `ModalAppointment` component to handle appointment scheduling and cancellation.
    - Utilized `Dialog` and `Button` components for modal structure and interaction.
    - Integrated `FormAppointmentSchedule` and `FormAppointmentCancel` for form handling within the modal.
    - Improved UI consistency with dynamic styling based on modal type.

---
+++ components/ModalAppointment.tsx
@@ -0,0 +1,66 @@
+import React from "react";
+import clsx from "clsx";
+import {Appointment} from "@/types/appwrite.types";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {Button} from "@/components/ui/button";
+import {FormAppointmentSchedule} from "@/components/forms/FormAppointmentSchedule";
+import {FormAppointmentCancel} from "@/components/forms/FormAppointmentCancel";
+
+type ModalAppointmentProps = {
+  type: "schedule" | "cancel";
+  appointmentId: string;
+  data: Appointment;
+};
+
+export function ModalAppointment(props: ModalAppointmentProps) {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Dialog
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <DialogTrigger asChild>
+        <Button
+          variant="ghost"
+          className={clsx("capitalize", {
+            "text-green-500": props.type === "schedule",
+          })}
+        >
+          {props.type}
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="shad-dialog sm:max-w-md">
+        <DialogHeader className="mb-4 space-y-3">
+          <DialogTitle className="capitalize">
+            {props.type} Appointment
+          </DialogTitle>
+          <DialogDescription>
+            Please fill in the following details to {props.type} an appointment
+          </DialogDescription>
+        </DialogHeader>
+        {props.type === "schedule" && (
+          <FormAppointmentSchedule
+            appointmentId={props.appointmentId}
+            data={props.data}
+            onSuccess={() => setOpen(false)}
+          />
+        )}
+        {props.type === "cancel" && (
+          <FormAppointmentCancel
+            appointmentId={props.appointmentId}
+            data={props.data}
+            onSuccess={() => setOpen(false)}
+          />
+        )}
+      </DialogContent>
+    </Dialog>
+  );
+}
